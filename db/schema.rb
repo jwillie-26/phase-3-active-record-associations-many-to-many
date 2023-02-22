80eea7c5db949677b78579ebaabe0d2a7914fc27
jwillie-26 <105637783+jwillie-26@users.noreply.github.com>
2023-02-22 09:18:37 +0300

    added solution

--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_19_113221) do
+ActiveRecord::Schema.define(version: 2022_11_08_162743) do
 
-  create_table "games", force: :cascade do |t|
-    t.string "title"
-    t.string "genre"
-    t.string "platform"
-    t.integer "price"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    create_table "games", force: :cascade do |t|
+      t.string "title"
+      t.string "genre"
+      t.string "platform"
+      t.integer "price"
+      t.datetime "created_at", precision: 6, null: false
+      t.datetime "updated_at", precision: 6, null: false
+    end
+  
+    create_table "reviews", force: :cascade do |t|
+      t.integer "score"
+      t.string "comment"
+      t.integer "game_id"
+      t.datetime "created_at", precision: 6, null: false
+      t.datetime "updated_at", precision: 6, null: false
+      t.integer "user_id"
+    end
+  
+    create_table "users", force: :cascade do |t|
+      t.string "name"
+      t.datetime "created_at", precision: 6, null: false
+      t.datetime "updated_at", precision: 6, null: false
+    end
+  
   end
-
-  create_table "reviews", force: :cascade do |t|
-    t.integer "score"
-    t.string "comment"
-    t.integer "game_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-end
+  
